42e7a3fd40fdd7ac71f1a0a9e1fa8bb0fcec1f7c
IbrahimMahmoud2023 <embabyibrahim95@gmail.com>
2025-09-11 23:01:42 +0300

    create custom app bar to home view body

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,10 +1,11 @@
+import 'package:bookely_app/features/home/presentation/view/wigdets/home_view_body.dart';
 import 'package:flutter/material.dart';
-
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: HomeViewBody(),
+    );
   }
 }
