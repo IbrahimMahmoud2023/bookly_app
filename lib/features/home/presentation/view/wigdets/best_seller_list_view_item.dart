611ed9d9500bc8fbb7f5358c1712481b01b084c4
IbrahimMahmoud2023 <embabyibrahim95@gmail.com>
2025-09-13 01:30:03 +0300

    best seller item part 3

---
+++ lib/features/home/presentation/view/wigdets/best_seller_list_view_item.dart
@@ -0,0 +1,62 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../../../constant.dart';
+import '../../../../../core/utils/assets_data.dart';
+import '../../../../../core/utils/style.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: 30),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Text(
+                  'Harry Potter and the Goblet of Fire',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20.copyWith(fontFamily: kGTSectraFine),
+                ),
+              ),
+              const SizedBox(height: 3),
+              Text(
+                'J.K. Rowling',
+                style: Styles.textStyle14.copyWith(
+                  color: Colors.white.withOpacity(0.7),
+                ),
+              ),
+              const SizedBox(height: 3),
+              Text(
+                  '19.99 €',
+                  style: Styles.textStyle20.copyWith(
+                    fontWeight: FontWeight.bold,
+                  )
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
