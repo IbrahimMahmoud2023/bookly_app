894b9a744d205cdb2030f6c4bca0665d64bd7ed9
IbrahimMahmoud2023 <embabyibrahim95@gmail.com>
2025-09-15 15:18:34 +0300

    add title and subtitle to book details

--- lib/features/home/presentation/view/wigdets/book_details_view_body.dart
+++ lib/features/home/presentation/view/wigdets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookely_app/core/utils/style.dart';
+import 'package:bookely_app/features/home/presentation/view/wigdets/book_rating.dart';
 import 'package:bookely_app/features/home/presentation/view/wigdets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
@@ -7,23 +9,36 @@ class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
   @override
   Widget build(BuildContext context) {
-var width = MediaQuery.of(context).size.width;
+    var width = MediaQuery.of(context).size.width;
     return Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 24),
-          child: Column(
-              children: [
-                CustomBookDetailsAppBar(),
-                Padding(
-                  padding:  EdgeInsets.symmetric(horizontal: width*0.22),
-                  child: CustomBookImage(),
-                ),
-              ]
-          ),
-        )
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 24),
+        child: Column(
+          children: [
+            CustomBookDetailsAppBar(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * 0.28),
+              child: CustomBookImage(),
+            ),
+            SizedBox(height: 43),
+            Text(
+              'The Jungle Book',
+              style: Styles.textStyle30.copyWith(fontWeight: FontWeight.w500),
+            ),
+            SizedBox(height: 6),
+            Opacity(
+              opacity: 0.7,
+              child: Text(
+                'Rudyard Kipling',
+                style: Styles.textStyle18.copyWith(fontWeight: FontWeight.w500),
+              ),
+            ),
+            SizedBox(height: 16),
+            // BookRating(),
+
+          ],
+        ),
+      ),
     );
   }
-
 }
-
-
