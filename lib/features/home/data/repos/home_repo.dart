9ca75b6d371fc47547d7909ceab7958070dfe30e
IbrahimMahmoud2023 <embabyibrahim95@gmail.com>
2025-09-16 00:29:19 +0300

    create home repo method and failure file

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,10 @@
+import 'package:bookely_app/core/errors/failures.dart';
+import 'package:bookely_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
+
+// dartz => ديه package قويه جدا بتخليني اقدر اقدر ارجع اكتر من حاجه في ال method
