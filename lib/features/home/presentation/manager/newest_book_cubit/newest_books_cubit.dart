2538eac2693ea170aab40bc455517b30382910b0
IbrahimMahmoud2023 <embabyibrahim95@gmail.com>
2025-09-16 22:11:51 +0300

    newest books cubit done

---
+++ lib/features/home/presentation/manager/newest_book_cubit/newest_books_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookely_app/features/home/data/repos/home_repo.dart';
+import 'package:meta/meta.dart';
+
+import '../../../data/models/book_model/book_model.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  final HomeRepo homeRepo;
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(failure.errorMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books));
+      },
+    );
+  }
+}
